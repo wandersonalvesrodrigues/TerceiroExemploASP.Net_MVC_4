a65a836e1492ccfd961966d6b828dd88b046befb
Wanderson Alves Rodrigues <wandersonallves@yahoo.com.br>
2016-02-19 23:33:35 -0200

    Criação do exemplo de validações.

---
+++ TerceiroExemploMVC/TerceiroExemploMVC/Controllers/PessoaController.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TerceiroExemploMVC.Models;
+
+namespace TerceiroExemploMVC.Controllers
+{
+    public class PessoaController : Controller
+    {
+        public ActionResult Index()
+        {
+            var pessoa = new Pessoa();
+            return View(pessoa);
+        }
+
+        [HttpPost]
+        public ActionResult Index(Pessoa pessoa)
+        {
+            /*if (string.IsNullOrEmpty(pessoa.Nome)) 
+            {
+                ModelState.AddModelError("nome", "Nome é obrigatório");
+            }
+
+            if (string.IsNullOrEmpty(pessoa.Senha)) 
+            {
+                ModelState.AddModelError("senha", "A senha é obrigatória");
+            }
+
+            if(!string.Equals(pessoa.Senha,pessoa.ConfirmarSenha))
+            {
+                ModelState.AddModelError("","As senhas não conferem");
+            }*/
+
+            if (ModelState.IsValid)
+            {
+                return View("Resultado", pessoa);
+            }
+
+            return View(pessoa);
+        }
+
+        public ActionResult Resultado(Pessoa pessoa)
+        {
+            return View(pessoa);
+        }
+
+        public ActionResult LoginUnico(string login)
+        {
+            var bancoNomes = new Collection<string> 
+            {
+                "João",
+                "Maria"
+            };
+
+            return Json(bancoNomes.All(x => x.ToLower() != login.ToLower()), JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ TerceiroExemploMVC/TerceiroExemploMVC/Models/Pessoa.cs
@@ -0,0 +1,24 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace TerceiroExemploMVC.Models
+{
+    public class Pessoa
+    {
+        [Required(ErrorMessage="O nome é Obrigatório")]
+        public string Nome { get; set; }
+        [StringLength(50,MinimumLength=5,ErrorMessage="A observação deve ficar entre 5 até 50 caracteres")]
+        public string Observacao { get; set; }
+        [Range(18,50,ErrorMessage="A pessoa deve possuir idade entre 18 até 50 anos")]
+        public int Idade { get; set; }
+        [Required(ErrorMessage="E-mail é obrigatório")]
+        [RegularExpression(@"^[A-Za-z0-9](([_.-]?[a-zA-Z0-9]+)*)@([A-Za-z0-9]+)(([.-]?[a-zA-Z0-9]+)*)([.][A-Za-z]{2,4})$",ErrorMessage="E-mail inválido")]
+        public string Email { get; set; }
+        [Required(ErrorMessage="Login é obrigatório")]
+        [System.Web.Mvc.Remote("LoginUnico","Pessoa",ErrorMessage="Pessoa já cadastrada")]
+        public string Login { get; set; }
+        [Required(ErrorMessage="A senha é obrigatória")]
+        public string Senha { get; set; }
+        [Required(ErrorMessage="O confirma senha é obrigatória")]
+        [Compare("Senha",ErrorMessage="As senhas não conferem")]
+        public string ConfirmarSenha { get; set; }
+    }
+}
